a6a0fccf2de101a0a61ed7c057f91b37b396ad4e
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-02-20 09:55:13 +0530

    Update variables.ts

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,10 +1,37 @@
 // variables
+
 let apples: number = 5;
 let speed: string = 'fast';
 let hasName: boolean = true;
 let nothingMuch: null = null;
-let nothing:undefined = undefined;
+let nothing: undefined = undefined;
 
 // built-in objects
 
-let now:Date = new Date();
+let now: Date = new Date();
+
+// Arrays
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4, 5, 6];
+
+
+// class
+class Car { }
+
+let car: Car = new Car();
+
+// object literaral 
+
+let point: { x: number; y: number } = {
+    x: 12,
+    y: 20
+}
+
+// function
+
+const logNumber:(i:number)=>void = (i:number)=>{
+    console.log(i);
+    
+}
+
